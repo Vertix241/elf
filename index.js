a8a579aa621edcb9c2b62fa6f4ec905f0442cad7
Vertix241 <94992327+Vertix241@users.noreply.github.com>
2022-01-29 19:19:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.on("messageCreate", message => {
     message.channel.send('https://tenor.com/view/ytpmv-elf-ytpmv-elf-gay-people-will-ferrell-gif-23475110')
   }
 
-  if (message.content === "p!Question") {
+  if (message.content === "p!question") {
     let questions = ['Whats your favorite colour', 'Do you like men', 'Whats your opinion on immigrants', 'Whats your opinion on black people', 'What are your views on the socio-economic climate on welfare economies in kyrgyzstan', 'Whats 9 + 10', 'Do you like polish people', 'What is', 'Do you really think that you will escape God\'s judgement for your sins'];
     
     let rnd = Math.floor(Math.random() * questions.length);
@@ -34,7 +34,12 @@ client.on("messageCreate", message => {
   if (message.content === "p!help") {
     let embed = new Discord.MessageEmbed()
     .setTitle("Commands")
-    .setDescription("p!question - asks a random question, p!joke - tell a joke, p!avatar <user> - self explanatory (sends the message author\'s avatar if no user is pinged)")
+    .setDescription('help')
+    .addField('p!elf', 'elf')
+    .addField('p!jokes', 'tell joke ver  funny')
+    .addField('p!question', 'WE DO NOT COLLECT ANY OF YOUR PERSONAL INFORMATION')
+    .addField('p!avatar', 'avatar')
+    .setColor('RANDOM')
 
     message.channel.send({embeds:[embed]});
   }
@@ -43,14 +48,14 @@ client.on("messageCreate", message => {
     message.channel.send('https://tenor.com/btNtV.gif')
   }
 
-  if (message.content === "p!joke") {
+  if (message.content.toLowerCase().startsWith('p!jokes')) {
     let jokes = ['Why did the pizza cross the road? It was being driven by a woman.', 'How far can you throw a pizza without it going in the oven? A mile.', 'A priest, a priest, and a priest were sitting at a table. Two men were sitting next to them at the table: a lawyer, a lawyer, and a lawyer. The lawyer said to the priest, \"I\ll be seeing you in court.\" The priest replied, \"I\'ll be seeing you in hell!\"', 'A lady walks into a bar. \"Sir, if you would please serve me a drink, I would appreciate it" The gentleman replies: \"Why, you seem to have', 'What do you call a Jewish woman masturbating? A Jew.', 'How did the Italian go? IDK', 'I really don\'t like my wife\'s dog. I\'m just too afraid to tell her', 'How many liberals does it take to fix a lightbulb? None, because they\'re too busy with their gender', 'Why did the man cross the road? So he could get to the other side', 'What do you call a woman who is pregnant? A pregnant woman.', 'Borax', 'P']
     let rnd = Math.floor(Math.random() * jokes.length);
 
     message.channel.send({content: jokes[rnd]});
   }
 
-   if (message.content.startsWith('p!avatar')) {
+   if (message.content.toLowerCase().startsWith('p!avatar')) {
     const user = message.mentions.users.first() || message.author;
     const avatarEmbed = new Discord.MessageEmbed()
     .setAuthor(user.username)
@@ -68,7 +73,7 @@ client.on('messageCreate', async message => {
     const background = await Canvas.loadImage('./elf.png');
     context.drawImage(background, 0, 0, canvas.width, canvas.height);
     const argument = await Canvas.loadImage(message.attachments.first().url)
-    context.drawImage(argument, 625, 80, 294, 303);
+    context.drawImage(argument, 625, 89, 294, 294);
     const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'red-cirlce.png');
 
     message.reply({files: [attachment]})
